511ae93e7d6ed76e4957ed8b242144124a767432
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-06-27 22:58:17 +0530

    Person Class

--- jsdoc-example/src/index.js
+++ jsdoc-example/src/index.js
@@ -19,6 +19,7 @@ const todo = {
     text: 'Hello'
 };
 
+
 /**
  * Calculate Tax
  * @param {number} amount - Total amount 
@@ -28,3 +29,59 @@ const todo = {
 const calculateTax = (amount, tax) => {
     return `$${amount + tax * amount}`;
 };
+
+/**
+ * Student Info
+ * @typedef {Object} Student
+ * @typedef {number} id - Student ID
+ * @typedef {string} name -Student Name
+ * @typedef {string|number} [age] - Student Age (optional) 
+ * @typedef {boolean} isActive - Student is Active 
+ */
+
+/**
+ * @type {Student}
+ */
+const Student = {
+    id: 1,
+    name: 'John Doe',
+    age: 20,
+    isActive: true
+}
+
+/**
+ * Class to create a person object
+ */
+class Person {
+  /**
+   * 
+   * @param {Object} personInfo Information about the person 
+   */
+  constructor(personInfo) {
+      /**
+       * @property {string} name Person Name
+       */
+      this.name = personInfo.name;
+      /**
+       * @property {string} name Person Age
+       */
+      this.age = personInfo.age
+  }
+  
+  /**
+   * @property {Function} greet A greeting with the name and age
+   * @returns void
+   */
+  greet() {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age}`);
+  }
+}
+
+/**
+ * Person Object
+ * See {@link Person}
+ */
+const personObject = new Person({
+    name: 'John Doe',
+    age: 30
+});
